f4d141f3a61392b2b5abe7f2d28b8ecd46e560f4
mayurdevops24 <mayur.mahajan3@tcs.com>
2025-02-19 12:40:53 +0530

    Delete action-two/index.js

--- action-two/index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  const name = core.getInput('name');
-  console.log(`Greetings, ${name} from Action Two!`);
-} catch (error) {
-  core.setFailed(error.message);
-}
